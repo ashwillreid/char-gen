7865c4178ed46155000e7746ffffc86818cdef88
ashwillReid <reid@rockpapercoin.com>
2024-04-10 09:29:03 -0700

    prettier has opinions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-		<div className='flex min-w-full justify-center'>
-			<Link href='/'>
-				<p className="text-5xl">Character Generator</p>
-			</Link>
-		</div>
-	)
+    <div className="flex min-w-full justify-center">
+      <Link href="/">
+        <p className="text-5xl">Character Generator</p>
+      </Link>
+    </div>
+  );
 };
 
-export default Header
+export default Header;
