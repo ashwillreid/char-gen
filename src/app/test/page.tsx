c164fc8c6d5455ccbf3114e8078f03f5d2d3c36a
ashwillReid <reid@rockpapercoin.com>
2024-04-10 14:23:47 -0700

    add param

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,7 @@
 export default async function Test() {
   let data;
   try {
-    const res = await fetch("http://localhost:3000", {
+    const res = await fetch("http://localhost:3000/testRouter/156", {
       method: "GET",
     });
     console.log("data?", data);
