ada54ec38ac756e1b6257eb16f3365bce595f598
ashwillReid <reid@rockpapercoin.com>
2024-04-10 09:26:19 -0700

    make header a link to root

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
 		<div className='flex min-w-full justify-center'>
-			<p className="text-5xl">Character Generator</p>
+			<Link href='/'>
+				<p className="text-5xl">Character Generator</p>
+			</Link>
 		</div>
 	)
 };
