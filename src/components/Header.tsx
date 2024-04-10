1fe83903aab2b41344bcc4153423714461b4c92a
ashwillReid <reid@rockpapercoin.com>
2024-04-10 10:39:19 -0700

    add start of topNav to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
+import Button from "./Button";
 
 const Header = () => {
   return (
-    <div className="flex min-w-full justify-center">
+    <div className="flex flex-col min-w-full items-center">
       <Link href="/" className="flex flex-direction-row">
         <Image src="/crest.png" alt="Icon" width={50} height={50} />
         <p className="text-5xl ml-2">Character Generator</p>
       </Link>
+      <div>
+        <Link href="/NewCharacter">
+          <Button text="New Character" />
+        </Link>
+      </div>
     </div>
   );
 };
