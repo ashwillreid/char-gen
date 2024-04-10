5f99731a4f4b25501d773d97714e6eacfe7ffabb
ashwillReid <reid@rockpapercoin.com>
2024-04-10 14:32:07 -0700

    lint

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,8 +4,8 @@ export default async function Test() {
     const res = await fetch("http://localhost:3000/testRouter/156", {
       method: "GET",
     });
-    console.log("data?", data);
     data = await res.json();
+    console.log("data?", data);
   } catch (error) {
     console.log(error);
   }
