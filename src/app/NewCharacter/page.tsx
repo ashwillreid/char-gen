4c0b8efa41c4070a5a8307f20134550283594fe8
ashwillReid <reid@rockpapercoin.com>
2024-04-10 10:26:24 -0700

    add beginning of new character page with two text fields

---
+++ src/app/NewCharacter/page.tsx
@@ -0,0 +1,10 @@
+import TextField from "@/components/Inputs/TextField";
+
+export default async function NewCharacter() {
+  return (
+    <>
+      <TextField placeholder="Name" />
+      <TextField placeholder="Test" />
+    </>
+  );
+}
