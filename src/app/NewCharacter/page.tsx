e2005716f95a92c3fa00f3c6802f74480691aff3
ashwillReid <reid@rockpapercoin.com>
2024-04-10 14:27:25 -0700

    add selectors

--- src/app/NewCharacter/page.tsx
+++ src/app/NewCharacter/page.tsx
@@ -1,13 +1,33 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TextField from "@/components/Inputs/TextField";
+import Select from "@/components/Inputs/Select";
+import { classOptions, raceOptions } from "../../../static/charOptions";
 
 export default function NewCharacter() {
   const [name, setName] = useState("");
+  const [charClass, setCharClass] = useState(classOptions[0].val);
+  const [race, setRace] = useState(raceOptions[0].val);
+
+  useEffect(() => {
+    console.log("STATE:", { name: name, class: charClass, race: race });
+  }, [charClass, name, race]);
 
   return (
     <>
       <TextField placeholder="Name" onUpdate={setName} val={name} />
+      <Select
+        label="Class"
+        options={classOptions}
+        val={charClass}
+        onUpdate={setCharClass}
+      />
+      <Select
+        label="Race"
+        options={raceOptions}
+        val={charClass}
+        onUpdate={setRace}
+      />
     </>
   );
 }
