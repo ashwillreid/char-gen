2991d2f90f6f454b5528db7f337d77a1b5070da9
ashwillReid <reid@rockpapercoin.com>
2024-04-10 09:29:34 -0700

    all prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Handjet } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 
-const handjet = Handjet( { subsets: ["latin"] } );
+const handjet = Handjet({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Character Generator",
@@ -17,14 +17,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={ handjet.className + ' ' + 'flex justify-center bg-gray-600'}>
-				<div className='container max-w-3xl'>
-					<div className="flex flex-col min-h-screen m-8 my-2 border-opacity-25 border-2 border-gray-500 border rounded p-2 outline-offset-1">
-						<Header />
-						{children}
-					</div>
-				</div>
-			</body>
+      <body
+        className={handjet.className + " " + "flex justify-center bg-gray-600"}
+      >
+        <div className="container max-w-3xl">
+          <div className="flex flex-col min-h-screen m-8 my-2 border-opacity-25 border-2 border-gray-500 border rounded p-2 outline-offset-1">
+            <Header />
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,18 +1,14 @@
 export default async function Test() {
-	let data;
-	try {
-		const res = await fetch('http://localhost:3000', {
-			method: 'GET',
-		});
-		console.log( 'data?', data )
-		data = await res.json()
-	} catch ( error ) {
-		console.log( error )
-	}
+  let data;
+  try {
+    const res = await fetch("http://localhost:3000", {
+      method: "GET",
+    });
+    console.log("data?", data);
+    data = await res.json();
+  } catch (error) {
+    console.log(error);
+  }
 
-  return (
-    <main>
-			{ data.message }
-		</main>
-  );
+  return <main>{data.message}</main>;
 }
