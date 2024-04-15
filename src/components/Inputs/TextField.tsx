edfed51ae52315da5ca666d874f71c0f935207d1
ashwillReid <reid@rockpapercoin.com>
2024-04-15 10:14:31 -0700

    fix color and size

--- src/components/Inputs/TextField.tsx
+++ src/components/Inputs/TextField.tsx
@@ -11,10 +11,10 @@ export default function TextField({
   onUpdate: Function;
 }>) {
   return (
-    <div className={`w-48 m-1 ${className ?? className}`}>
+    <div className={`w-64 m-1 ${className ?? className}`}>
       <input
         type="text"
-        className="block w-full rounded-md border-0 py-1.5 pl-2 pr-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
+        className="block text-2xl w-full rounded-md border-0 py-1.5 pl-2 pr-2 text-gray-600 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600"
         placeholder={placeholder}
         value={val}
         onChange={(e) => {
