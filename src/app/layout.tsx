34663a262dc6a8e92bfe12a64832279927c9c7be
ashwillReid <reid@rockpapercoin.com>
2024-04-15 09:54:15 -0700

    bigger paper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body
         className={handjet.className + " " + "flex justify-center bg-gray-600"}
       >
-        <div className="container max-w-3xl">
+        <div className="container max-w-5xl">
           <div className="flex flex-col min-h-screen m-8 my-2 border-opacity-25 border-2 border-gray-500 border rounded p-2 outline-offset-1">
             <Header />
             {children}
