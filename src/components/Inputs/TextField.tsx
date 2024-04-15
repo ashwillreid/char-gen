e131a4d950a1415426703550542aa4c397d5eb93
ashwillReid <reid@rockpapercoin.com>
2024-04-15 10:44:14 -0700

    style unification

--- src/components/Inputs/TextField.tsx
+++ src/components/Inputs/TextField.tsx
@@ -14,7 +14,7 @@ export default function TextField({
     <div className={`w-64 m-1 ${className ?? className}`}>
       <input
         type="text"
-        className="block text-2xl w-full rounded-md border-0 py-1.5 pl-2 pr-2 text-gray-600 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600"
+        className="block bg-gray-700 text-2xl w-full rounded-md py-1.5 pl-2 pr-2 ring-1 ring-inset ring-gray-500 placeholder:text-gray-300 focus:ring-2 focus:ring-inset focus:ring-gray-600"
         placeholder={placeholder}
         value={val}
         onChange={(e) => {
