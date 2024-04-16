16c3910e79ed1668c554f40c6a330b5dd94bd4f8
ashwillReid <reid@rockpapercoin.com>
2024-04-16 16:43:51 -0700

    caps

--- static/charOptions.ts
+++ static/charOptions.ts
@@ -1,9 +1,9 @@
 export const classOptions = [
-  { val: "Barbarian", text: "Barbarian" },
-  { val: "Rouge", text: "Rouge" },
+  { val: "BARBARIAN", text: "Barbarian" },
+  { val: "ROUGE", text: "Rouge" },
 ];
 
 export const raceOptions = [
-  { val: "Elf", text: "Elf" },
-  { val: "Orc", text: "Orc" },
+  { val: "ELF", text: "Elf" },
+  { val: "ORC", text: "Orc" },
 ];
