02edbd14b7f174583cae50785d78604e3f2da370
ashwillReid <reid@rockpapercoin.com>
2024-04-16 10:45:42 -0700

    playground

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,15 +1,32 @@
+import { useRouter } from "next/navigation";
 import { mutation } from "../../../static/helpers/mutation";
 import { query } from "../../../static/helpers/query";
 import { useEffect } from "react";
-export default async function Test() {
+
+type Character = {
+  name: string;
+  age: number;
+  race: string;
+  charClass: string;
+};
+
+export default async function Test({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
   // const args = {
   //   name: "testName",
   //   age: 42,
   //   race: "ELF",
   //   charClass: "ROUGE",
   // };
-  const test = await query("character/get/clv1k1ine000212zkykf27tzr");
+
+  console.log("search params?", searchParams);
+  const test = await query("character/clv1k1ine000212zkykf27tzr");
   console.log("THIS IS A TEST", test);
-  return <main>{JSON.stringify(test)}</main>;
+  return <main>{JSON.stringify(test.name)}</main>;
 }
 // clv1k1ine000212zkykf27tzr
