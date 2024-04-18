d842f635ed1adf5d0faba40d035564ba2df4c7d4
ashwillReid <reid@rockpapercoin.com>
2024-04-18 09:19:59 -0700

    lint

--- src/API/mutation/character/createCharacter.ts
+++ src/API/mutation/character/createCharacter.ts
@@ -1,7 +1,3 @@
-import Select from "@/components/Inputs/Select";
-import { classOptions, raceOptions } from "../../../../static/charOptions";
-import FormRow from "@/components/FormRow";
-import Button from "@/components/Button";
 import { mutation } from "../../../../static/helpers/mutation";
 
 export const createCharacter = async ({
