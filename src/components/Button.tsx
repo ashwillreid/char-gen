e6857a2afb1a0723c1eb1e8958934f19c8baea9b
ashwillReid <reid@rockpapercoin.com>
2024-04-19 08:50:15 -0700

    add warning and submit colorways for reusable button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,25 @@
 import { MouseEventHandler } from "react";
 
 export default function Button({
-  className,
+  className = "",
   text,
   clickHandler,
+  type,
 }: Readonly<{
   className?: string;
   text: string;
   clickHandler?: MouseEventHandler;
+  type?: { warning?: boolean; submit?: boolean };
 }>) {
+  if (type?.warning) {
+    className += "bg-red-500";
+  }
+  if (type?.submit) {
+    className += "bg-green-900";
+  }
   return (
     <button
-      className={`flex items-center hover:bg-gray-400  py-2 px-4 rounded h-6 ${className ?? className}`}
+      className={`m-1 flex items-center hover:bg-gray-400  py-2 px-4 rounded h-6 ${className ?? className}`}
       onClick={clickHandler ? clickHandler : undefined}
     >
       <span>{text}</span>
